6101c16862a6e8238a3e0106a8c74086697f4d32
Alan <alanfernande8@gmail.com>
2021-10-26 15:41:05 -0300

    segundo deploit

--- src/keys.js
+++ src/keys.js
@@ -1,9 +1,9 @@
 module.exports = {
     database: {
         connectionLimit: 10,
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'database_links'
+        host: 'bflcaq4yqm0axd8usdvh-mysql.services.clever-cloud.com',
+        user: 'uckuxtm4dbnfg0f9',
+        password: 'uckuxtm4dbnfg0f9',
+        database: 'bflcaq4yqm0axd8usdvh'
     }
 };
