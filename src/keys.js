abb0b6734023add20889ab4f0badb59750959152
Alan <alanfernande8@gmail.com>
2021-10-26 16:22:37 -0300

    changes on keys

--- src/keys.js
+++ src/keys.js
@@ -3,7 +3,7 @@ module.exports = {
         connectionLimit: 10,
         host: 'bflcaq4yqm0axd8usdvh-mysql.services.clever-cloud.com',
         user: 'uckuxtm4dbnfg0f9',
-        password: 'uckuxtm4dbnfg0f9',
+        password: 'd8FKgP9dnM9kjzndbftT',
         database: 'bflcaq4yqm0axd8usdvh'
     }
 };
