b6463f7f1066533e031e5a3d9d376e42ba05d3bd
Alan <alanfernande8@gmail.com>
2021-11-24 19:08:09 -0300

    deploit heroku whit leaflet

--- src/keys.js
+++ src/keys.js
@@ -1,9 +1,9 @@
 module.exports = {
     database: {
         connectionLimit: 10,
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'database_links'
+        host: 'bflcaq4yqm0axd8usdvh-mysql.services.clever-cloud.com',
+        user: 'uckuxtm4dbnfg0f9',
+        password: 'd8FKgP9dnM9kjzndbftT',
+        database: 'bflcaq4yqm0axd8usdvh'
     }
 };
